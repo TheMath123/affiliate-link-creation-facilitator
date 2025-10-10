fa0abae108687522d81cb1a046a47bf4f77abcdb
Matheus Agostinho <contact@matheuspa.com>
2025-10-10 13:19:26 -0300

    feat: update GEMINI_MODEL default value and clean up environment variables

--- src/env.ts
+++ src/env.ts
@@ -5,19 +5,12 @@ export const env = createEnv({
   server: {
     APP_URL: z.string().min(1),
     GEMINI_API_KEY: z.string().min(1),
-    GEMINI_MODEL: z.string().min(1).default("gemini-1.5-flash"),
+    GEMINI_MODEL: z.string().min(1).default("gemini-2.0-flash-001"),
   },
-  client: {
-    NEXT_PUBLIC_BETTER_STACK_SOURCE_TOKEN: z.string().min(1),
-    NEXT_PUBLIC_BETTER_STACK_INGESTING_URL: z.string().min(1),
-  },
+  client: {},
   runtimeEnv: {
     APP_URL: process.env.APP_URL,
     GEMINI_API_KEY: process.env.GEMINI_API_KEY,
     GEMINI_MODEL: process.env.GEMINI_MODEL,
-    NEXT_PUBLIC_BETTER_STACK_SOURCE_TOKEN:
-      process.env.NEXT_PUBLIC_BETTER_STACK_SOURCE_TOKEN,
-    NEXT_PUBLIC_BETTER_STACK_INGESTING_URL:
-      process.env.NEXT_PUBLIC_BETTER_STACK_INGESTING_URL,
   },
 });
